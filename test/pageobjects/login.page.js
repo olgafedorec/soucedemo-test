9d15c2374c492807bf8fc7bc80220a8bb5c09ae7
Olga Fedorec <olga_fedorec@yahoo.com>
2025-08-13 21:30:14 +0300

    Update login.page.js

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -4,33 +4,27 @@ class LoginPage extends Page {
     get usernameInput () {
         return $('#user-name');
     }
-
     get passwordInput () {
         return $('#password');
     }
-
     get loginBtn () {
         return $('#login-button');
     }
-
     get errorMessage () {
         return $('.error-message-container');
     }
-
     async open () {
         return super.open('');
     }
-
     async login (username, password) {
         await this.usernameInput.setValue(username);
         await this.passwordInput.setValue(password);
         await this.loginBtn.click();
     }
-
     async getErrorText() {
         return await this.errorMessage.getText();
     }
-
 }
+
 const loginPage = new LoginPage();
 module.exports = loginPage;
